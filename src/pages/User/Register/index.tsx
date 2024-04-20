1625cf17ec4bc0af8fac565d6bd8abb935ce9a90
starlightcxy <3050094865@qq.com>
2024-04-20 13:17:47 +0800

    加入

---
+++ src/pages/User/Register/index.tsx
@@ -0,0 +1,194 @@
+import {Footer} from '@/components';
+import {login, register} from '@/services/ant-design-pro/api';
+import {LockOutlined, UserOutlined,} from '@ant-design/icons';
+import {LoginForm, ProFormText,} from '@ant-design/pro-components';
+import {Helmet, history} from '@umijs/max';
+import {Alert, message, Tabs} from 'antd';
+import {createStyles} from 'antd-style';
+import React, {useState} from 'react';
+import Settings from '../../../../config/defaultSettings';
+
+const useStyles = createStyles(({token}) => {
+  return {
+    action: {
+      marginLeft: '8px',
+      color: 'rgba(0, 0, 0, 0.2)',
+      fontSize: '24px',
+      verticalAlign: 'middle',
+      cursor: 'pointer',
+      transition: 'color 0.3s',
+      '&:hover': {
+        color: token.colorPrimaryActive,
+      },
+    },
+    lang: {
+      width: 42,
+      height: 42,
+      lineHeight: '42px',
+      position: 'fixed',
+      right: 16,
+      borderRadius: token.borderRadius,
+      ':hover': {
+        backgroundColor: token.colorBgTextHover,
+      },
+    },
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    },
+  };
+});
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const {styles} = useStyles();
+  // 表单提交
+  const handleSubmit = async (values: API.RegisterParams) => {
+    try {
+      //校验
+      const {userPassword, checkPassword} = values;
+      if(userPassword !== checkPassword){
+        message.error('两次输入的密码不一致');
+        return;
+      }
+
+      // 注册
+      const id = await register(values);
+
+      if (id > 0) {
+        const defaultLoginSuccessMessage = '注册成功！';
+        message.success(defaultLoginSuccessMessage);
+
+        // const urlParams = new URL(window.location.href).searchParams;
+        //没有redirect的那段逻辑，不能是如下了。
+        // history.push('/user/login?redirect=' + redirect);
+        history.push('/user/login');
+        return;
+      }else{
+        throw new Error(`register error id = ${id}`)
+      }
+
+    } catch (error) {
+      const defaultLoginFailureMessage = '注册失败，请重试！';
+      // console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <Helmet>
+        <title>
+          {'注册'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          submitter={{
+            searchConfig: {
+              submitText: '注册'
+            }
+          }}
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg"/>}
+          title="rainc的用户中心"
+          subTitle={'Yes'}
+          initialValues={{
+            autoLogin: true,
+          }}
+          onFinish={async (values) => {
+            await handleSubmit(values as API.RegisterParams);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '账号密码注册',
+              },
+            ]}
+          />
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined/>,
+                }}
+                placeholder="请输入账号"
+                rules={[
+                  {
+                    required: true,
+                    message: '账号是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined/>,
+                }}
+                placeholder="请输入密码"
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                  {
+                    min: 8,
+                    type: 'string',
+                    message: '密码最少8位',
+                  }
+                ]}
+              />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined/>,
+                }}
+                placeholder="请重复密码"
+                rules={[
+                  {
+                    required: true,
+                    message: '确认密码是必填项！',
+                  },
+                  {
+                    min: 8,
+                    type: 'string',
+                    message: '密码最少8位',
+                  }
+                ]}
+              />
+            </>
+          )}
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+          </div>
+        </LoginForm>
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+export default Register;
